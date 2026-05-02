6c4862532b29b50dcbae1199d3561e2906cf8e46
Kings-Israel <milimokings@gmail.com>
2026-05-02 09:08:51 +0300

    Updated Booking analyzer

--- app/AiAgents/BookingAnalyzer.php
+++ app/AiAgents/BookingAnalyzer.php
@@ -15,53 +15,55 @@ class BookingAnalyzer extends Agent
     protected $tools = [];
 
     protected $responseSchema = [
-        'event_name' => [
-            'type' => 'string',
-            'description' => 'The name of the event or booking.'
-        ],
-        'event_type' => [
-            'type' => 'string',
-            'description' => 'The type of the event or booking. Can be either "Photo Shoot", "Video Shoot" or "Photo and Video Shoot".'
-        ],
-        'event' => [
-            'type' => 'string',
-            'description' => 'The specific event or booking type. If the event type is "Photo Shoot", the event can be either "Portrait", "Fashion", "Product", "Event" or "Other". If the event type is "Video Shoot", the event can be either "Commercial", "Music Video", "Documentary", "Event" or "Other". If the event type is "Photo and Video Shoot", the event can be either "Commercial", "Music Video", "Documentary", "Event" or "Other".'
-        ],
-        'event_duration' => [
-            'type' => 'string',
-            'description' => 'The duration of the event or booking. Can be either "Less than 1 hour",
-                "1-2 hours",
-                "2-3 hours",
-                "3-4 hours",
-                "4-5 hours",
-                "5-6 hours",
-                "6-12 hours" or
-                "Over 12 hours"'
-        ],
-        'event_date' => [
-            'type' => 'string',
-            'description' => 'The date of the event or booking.'
-        ],
-        'event_location' => [
-            'type' => 'string',
-            'description' => 'The location of the event or booking.'
-        ],
-        'event_duration' => [
-            'type' => 'string',
-            'description' => 'The duration of the event or booking.'
-        ],
-        'number_of_outfit_changes' => [
-            'type' => 'integer',
-            'description' => 'The number of outfit changes the customer will do.'
-        ],
-        'number_of_people' => [
-            'type' => 'integer',
-            'description' => 'The number of people attending the event or booking.'
-        ],
-        'event_description' => [
-            'type' => 'string',
-            'description' => 'A brief description of the event or booking.'
-        ],
+        'name' => 'booking_analysis',
+        'schema' => [
+            'type' => 'object',
+            'properties' => [
+                'event_name' => [
+                    'type' => 'string',
+                    'description' => 'The name of the event or booking.'
+                ],
+                'event_type' => [
+                    'type' => 'string',
+                    'description' => 'The type of the event or booking. Can be either "Photo Shoot", "Video Shoot" or "Photo and Video Shoot".'
+                ],
+                'event' => [
+                    'type' => 'string',
+                    'description' => 'The specific event or booking type. If the event type is "Photo Shoot", the event can be either "Portrait", "Fashion", "Product", "Event" or "Other". If the event type is "Video Shoot", the event can be either "Commercial", "Music Video", "Documentary", "Event" or "Other". If the event type is "Photo and Video Shoot", the event can be either "Commercial", "Music Video", "Documentary", "Event" or "Other".'
+                ],
+                'event_duration' => [
+                    'type' => 'string',
+                    'description' => 'The duration of the event or booking. Can be either "Less than 1 hour",
+                        "1-2 hours",
+                        "2-3 hours",
+                        "3-4 hours",
+                        "4-5 hours",
+                        "5-6 hours",
+                        "6-12 hours" or
+                        "Over 12 hours"'
+                ],
+                'event_date' => [
+                    'type' => 'string',
+                    'description' => 'The date of the event or booking.'
+                ],
+                'event_location' => [
+                    'type' => 'string',
+                    'description' => 'The location of the event or booking.'
+                ],
+                'number_of_outfit_changes' => [
+                    'type' => 'integer',
+                    'description' => 'The number of outfit changes the customer will do.'
+                ],
+                'number_of_people' => [
+                    'type' => 'integer',
+                    'description' => 'The number of people attending the event or booking.'
+                ],
+                'event_description' => [
+                    'type' => 'string',
+                    'description' => 'A brief description of the event or booking.'
+                ],
+            ]
+        ]
     ];
 
     public function instructions()
